73ab8316c83b40a5cdf0cd3387f72943600eb358
yewonahn <yewonahn@naver.com>
2024-03-12 19:46:27 +0900

    #2 : 2167번 2차원 배열의 합
    #2 : Implementation [Try]

---
+++ 구현/2167_2차원 배열의 합.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().strip().split())
+lst = [[0] * (m + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    nums = list(map(int, input().strip().split()))
+    for e in range(1, m + 1):
+        lst[a][e] = nums[e - 1]
+
+k = int(input())
+for b in range(k):
+    i, j, x, y = map(int, input().strip().split())
+    result = 0
+    if i == x and j != y:
+        for c in range(j, y + 1):
+            result += lst[i][c]
+    elif i != x and j == y:
+        for c in range(i, x + 1):
+            result += lst[c][j]
+    elif i == x and j == y:
+        result = lst[i][j]
+    else:
+        for c in range(i, x + 1):
+            for d in range(j, y + 1):
+                result += lst[c][d]
+    print(result)
