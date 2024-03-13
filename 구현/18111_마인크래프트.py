c82d58d36a546733e05fc15939525099eba000fa
yewonahn <yewonahn@naver.com>
2024-03-13 20:13:49 +0900

    #2 : 18111번 마인크래프트
    #2 : Implementation [풀이 참조, PyPy3 통과]

--- 구현/18111_마인크래프트.py
+++ 구현/18111_마인크래프트.py
@@ -10,29 +10,25 @@ height = [list(map(int, input().strip().split())) for _ in range(n)]
 
 # 변화 값 : 높이 (범위) 0 <= 높이 <= 256
 # 구해야 할 것 : (최소) 시간
-times = {}
+time = sys.maxsize  # 최대 높이 비교 size
+max_h = 0
 # 모든 높이 순회
-for fix in range(0, 257):
+for fix in range(257):
     # (높이 고정) -> 모든 칸 순회
-    time = 0
+    remove_block, add_block = 0, 0
     for j in range(n):
         for i in range(m):
-            # 시간 구하기
-            h = height[j][i]
             # 1. 블록 제거
-            if h - fix >= 0:
-                time += (h - fix) * 2
-            # 블록 추가
-            # 2. 시간 구하기
+            if height[j][i] - fix > 0:
+                remove_block += (height[j][i] - fix)
+            # 2. 블록 추가
             else:
-                time += fix - h
-    if time in times:
-        if fix > times[time]:
-            times[time] = fix
-    else:
-        times[time] = fix
+                add_block += (fix - height[j][i])
+    # 조건) 제거 블록 + b >= 추가 블록
+    if remove_block + b >= add_block:
+        # 최대 높이 저장
+        if remove_block * 2 + add_block <= time:
+            time = remove_block * 2 + add_block
+            max_h = fix
 
-result_t = min(times.keys())
-result_h = times[result_t]
-
-print(result_t, result_h)
+print(time, max_h)
