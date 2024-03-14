76f2e43f8d24e2de339950e6cfed3320175e9778
yewonahn <yewonahn@naver.com>
2024-03-14 19:06:39 +0900

    #1 : 1697번 숨바꼭질(BFS)
    #1 : 그래프 탐색 [풀이 참조]

--- 탐색/1697_숨바꼭질.py
+++ 탐색/1697_숨바꼭질.py
@@ -1,35 +1,25 @@
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
+
+def bfs():
+    q = deque()
+    q.append(n)
+
+    while q:
+        now = q.popleft()
+
+        if now == k:
+            return
+        for next in (now-1, now+1, 2*now):
+            if 0 <= next <= 100000 and visited[next] == 0:
+                visited[next] = visited[now] + 1
+                q.append(next)
+
 
 # 수빈, 동생
 n, k = map(int, input().split())
+visited = [0] * 100001
 
-# 구해야 하는 것 : 최소 시간
-# (탐색) 이동할 수 있는 모든 점 구하기
-# 임의의 점까지 이동할 수 있는 최소 시간 누적 or new 최소 시간으로 변경
-visited = [0] * 100001  # (d < 0) IndexError로 추가
-next = deque()
-
-# 자신, 부모
-next.append([n, n])
-while len(next) != 0:
-    # 현재 점 위치
-    n = next[0][0]
-    # print('n :', n)
-    next.popleft()
-    # 현재 점에서 이동할 수 있는 점 케이스
-    d = [n - 1, n + 1, 2 * n]
-    for i in d:
-        if i < 0 or i > 100000:
-            continue
-        if visited[i] == 0:
-            # print('차례 : ', i)
-            next.append([i, n])
-            visited[i] = visited[n] + 1
-        else:
-            if visited[i] > visited[n] + 1:
-                visited[i] = visited[n] + 1
-
-# print(visited)
+bfs()
 print(visited[k])
