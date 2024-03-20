4212fc04f4305b139a15bcbd722ee99a5a9d68b1
yewonahn <yewonahn@naver.com>
2024-03-20 13:55:09 +0900

    #2 : 3085번 사탕 게임
    #2 : Implementation

--- 구현/3085_사탕 게임.py
+++ 구현/3085_사탕 게임.py
@@ -10,68 +10,48 @@ board = [list(map(str, input().strip())) for _ in range(n)]
 # 먹을 수 있는 최대 개수
 # 3 ≤ N ≤ 50 -> 완전 탐색
 
-# 가로 확인
-def check_w(x, y):
-    global cnt
-    if x + 1 < n and y < n and board[y][x] == board[y][x + 1]:
-        cnt += 1
-        check_w(x+1, y)
-
-# 세로 확인
-def check_l(x, y):
-    global cnt
-    if y + 1 < n and x < n and board[y][x] == board[y+1][x]:
-        cnt += 1
-        check_l(x, y+1)
-
 def count():
-    global max, cnt
+    global max
     for a in range(n):
+        # cnt 0 이 아닌 1 부터 시작
+        cnt = 1
+        # 가로 확인
         for b in range(n-1):
-            # (b, a) 현재 테스트 하는 점
-            # 가로 확인
-            #print('현재 테스트 하는 점 : ', b, ',', a)
-            cnt = 1
-            check_w(b, a)
+            # range(n-1) 이용해서, 가로 세로 인덱스 다르게 설정
+            if board[a][b] == board[a][b+1]:
+                cnt += 1
+            else:
+                # 다음 칸에 다른 숫자 들어 있으면 초기화
+                cnt = 1
             if cnt > max:
-                #print('가로 확인 cnt > max : ', cnt, ',', max)
                 max = cnt
-            # 세로 확인
-            cnt = 1
-            check_l(b, a)
+
+        cnt = 1
+        # 세로 확인
+        for b in range(n - 1):
+            # range(n-1) 이용해서, 가로 세로 인덱스 다르게 설정
+            if board[b][a] == board[b+1][a]:
+                cnt += 1
+            else:
+                # 다음 칸에 다른 숫자 들어 있으면 초기화
+                cnt = 1
             if cnt > max:
-                #print('check_l', b, a)
-                #print('세로 확인 cnt > max : ', cnt, ',', max)
                 max = cnt
 
 
 max = 0
-cnt = 0
 for i in range(n):
     for j in range(n-1):
         # 가로 교환 (오른쪽)
         if j+1 < n and board[i][j] != board[i][j+1]:
-            tmp = board[i][j]
-            board[i][j] = board[i][j+1]
-            board[i][j + 1] = tmp
-            #print('--change board (가로)--')
-            #print(board)
+            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
             count()
             # 원상 복귀
-            tmp = board[i][j]
-            board[i][j] = board[i][j + 1]
-            board[i][j + 1] = tmp
-            # 세로 교환 (아래)
+            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+        # 세로 교환 (아래)
         if i+1 < n and board[i][j] != board[i+1][j]:
-            tmp = board[i][j]
-            board[i][j] = board[i+1][j]
-            board[i+1][j] = tmp
-            #print('--change board (세로)--')
-            #print(board)
+            board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
             count()
             # 원상 복귀
-            tmp = board[i][j]
-            board[i][j] = board[i + 1][j]
-            board[i + 1][j] = tmp
-
+            board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
 print(max)
