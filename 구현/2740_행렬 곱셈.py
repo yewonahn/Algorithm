239c7d67057722ed6db221beea5a03955bb7c454
yewonahn <yewonahn@naver.com>
2024-03-25 02:05:33 +0900

    #2 : 2740번 행렬 곱셈
    #2 : Implementation [행렬 곱셈 방법 참조]

--- 구현/2740_행렬 곱셈.py
+++ 구현/2740_행렬 곱셈.py
@@ -1,37 +1,22 @@
+# 행렬 곱셈 방법 참조
 import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-first = []
-for i in range(n):
-    first.append(list(map(int, input().strip().split())))
+A = [list(map(int, input().strip().split())) for _ in range(n)]
+m, k = map(int, input().split())
+B = [list(map(int, input().strip().split())) for _ in range(m)]
+C = [[0] * k for _ in range(n)]
 
-second = []
-# p = m
-p, k = map(int, input().split())
-for i in range(m):
-    second.append(list(map(int, input().strip().split())))
+# y축 갱신
+for y in range(n):
+    # x축 갱신
+    for x in range(k):
+        # 더하기 루프 하나
+        for p in range(m):
+            C[y][x] += A[y][p] * B[p][x]
 
-cnt = k * m
-for i in range(m):
-    cnt -= second[i].count(0)
-#print(cnt)
-result = [[0] * cnt for _ in range(n)]
-#print(result)
-
-# first : [m][n] / second : [m][k]
-x = 0
-for a in range(m):
-    for b in range(k):
-        if second[a][b] != 0:
-            #print('second[a][b] : ', second[a][b])
-            for c in range(n):
-                #p = first[c][a] * second[a][b]
-                #print(first[c][a] * second[a][b])
-                result[c][x] = first[c][a] * second[a][b]
-            x += 1
-
-for i in result:
-    for j in i:
-        print(j, end=' ')
+for y in range(n):
+    for x in range(k):
+        print(C[y][x], end=' ')
     print()
