be901ca6f574c99e83daa6268820e64c01fc6cf1
yewonahn <yewonahn@naver.com>
2024-03-27 16:35:05 +0900

    #2 : 1213번 팰린드롬 만들기
    #2 : Implementation [풀이 참조]

--- 구현/1213_팰린드롬 만들기.py
+++ 구현/1213_팰린드롬 만들기.py
@@ -1,39 +1,32 @@
-# Counter, sorted 참조
+# [풀이 참조]
+# Counter, sorted, 리스트.items(), 리스트 역순 접근 참조
 
 from collections import Counter
 import sys
 input = sys.stdin.readline
 
 name = list(input().strip())
+# Counter : 각 원소가 몇 번씩 나오는지 저장된 딕셔너리 객체 반환
 alphb = Counter(name)
-
-check = False
-charac = ''
 # 홀수인 알파벳 개수
 cnt = 0
+mid = ''
+result = ''
+
 for k, v in list(alphb.items()):
     if v % 2 == 1:
         cnt += 1
-        charac = k
-        check = True
+        mid = k
         # 1. 홀수 개인 알파벳이 두 개 이상 -> 안됨
         if cnt >= 2:
             print("I'm Sorry Hansoo")
-            # 종료
             exit()
 
-# sorted(alphb.items()) -> list
+# sorted(alphb.items()) -> list 반환
+# sorted(alphb.items()) 결과 리스트 [key, value] 가 저장되어 있음
+print(alphb.items())
 for k, v in sorted(alphb.items()):
-    if k != charac:
-        print(k * (v // 2), end='')
-    # 2. 홀수 개인 알파벳 한 개 -> 한 개만 가운데 출력
-    else:
-        print(k * (v // 2), end='')
-if check:
-    print(charac, end='')
-for k, v in sorted(alphb.items(), reverse=True):
-    if k != charac:
-        print(k * (v // 2), end='')
-    # 2. 홀수 개인 알파벳 한 개 -> 한 개만 가운데 출력
-    else:
-        print(k * (v // 2), end='')
+    result += (k * (v // 2))
+
+# result[::-1] : 리스트 역순 접근
+print(result + mid + result[::-1])
