0bd8fcb70616a3de6980c9f671d197887bbb0be3
yewonahn <yewonahn@naver.com>
2024-03-27 16:02:53 +0900

    #2 : 1213번 팰린드롬 만들기
    #2 : Implementation [맞음 / Counter, sorted 참조]

--- 구현/1213_팰린드롬 만들기.py
+++ 구현/1213_팰린드롬 만들기.py
@@ -11,10 +11,10 @@ check = False
 charac = ''
 # 홀수인 알파벳 개수
 cnt = 0
-for key in alphb:
-    if alphb[key] % 2 == 1:
+for k, v in list(alphb.items()):
+    if v % 2 == 1:
         cnt += 1
-        charac = key
+        charac = k
         check = True
         # 1. 홀수 개인 알파벳이 두 개 이상 -> 안됨
         if cnt >= 2:
@@ -22,17 +22,18 @@ for key in alphb:
             # 종료
             exit()
 
-for key in sorted(alphb.keys()):
-    if key != charac:
-        print(key * (alphb[key] // 2), end='')
+# sorted(alphb.items()) -> list
+for k, v in sorted(alphb.items()):
+    if k != charac:
+        print(k * (v // 2), end='')
     # 2. 홀수 개인 알파벳 한 개 -> 한 개만 가운데 출력
     else:
-        print(key * (alphb[key] // 2), end='')
+        print(k * (v // 2), end='')
 if check:
     print(charac, end='')
-for key in sorted(alphb.keys(), reverse=True):
-    if key != charac:
-        print(key * (alphb[key] // 2), end='')
+for k, v in sorted(alphb.items(), reverse=True):
+    if k != charac:
+        print(k * (v // 2), end='')
     # 2. 홀수 개인 알파벳 한 개 -> 한 개만 가운데 출력
     else:
-        print(key * (alphb[key] // 2), end='')
+        print(k * (v // 2), end='')
